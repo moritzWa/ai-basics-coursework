f871d33ad35283c3630a3b2ac24841110edc393c
Moritz Wallawisch <wallawitsch@gmail.com>
2020-10-17 18:27:58 +0200

    utility

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -112,7 +112,9 @@ def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
-
+    if winner(board) == X: return 1
+    if winner(board) == O: return -1
+    if winner(board) == False: return 0
 
 def minimax(board):
     """
